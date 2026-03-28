7e254cc6b68961723b636717202096cd3cc3289f
ragnito <ronaldo.aguirre@protonmail.com>
2026-03-29 00:26:20 +0100

    Remove unused LibSQLStore import from mastra/index.ts

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,6 +1,5 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
-import { LibSQLStore } from "@mastra/libsql";
 import {
   Observability,
   DefaultExporter,
